04b3ee97ed5d07aa9d2fb95e78d74477246a5aa7
jiawulu <jiawu.lu@gmail.com>
2012-03-22 11:58:53 +0800

    update

--- js/zepto/test.js
+++ js/zepto/test.js
@@ -1,31 +1,112 @@
-$.showOs = function(){
+$.showOs = function() {
 	//alert(navigator.userAgent)
 	console.log($.os);
 	console.log(navigator.userAgent);
 	return this;
 }
 
-$.localSotrage = function(){	
-	 if (window.localStorage) {
-      console.log('This browser supports localStorage');
-      //Do something with localStorage
-      window.localStorage.myLocalKey = 'Local example';
-      console.log(window.localStorage.myLocalKey);
-    } else {
-      alert('This browser does NOT support localStorage');
-    }
-    
-    window.localStorage.setItem('hello','world');
+$.localSotrage = function() {
+	if(window.localStorage) {
+		console.log('This browser supports localStorage');
+		//Do something with localStorage
+		window.localStorage.myLocalKey = 'Local example';
+		console.log(window.localStorage.myLocalKey);
+	} else {
+		alert('This browser does NOT support localStorage');
+	}
 }
 
-  // display the contents of a storage event
-  function displayStorageEvent(e) {
-  	alert(e)
-    var logged = "key:" + e.key + ", newValue:" + e.newValue + ", oldValue:" +
-                 e.oldValue +", url:" + e.url + ", storageArea:" + e.storageArea;
+$.middleJump = function() {
+	var currentUrl = window.location.href;
+	var appIds = ['sid', 'tks'];
+	console.log(window.location.href)
+	console.log(window.location.search)
 
-    alert('EVENT: ' + logged);
-  }
+	//TODO
+	storageAppData();
+	storageMonitorData();
 
-  // add a storage event listener for this origin
-  window.addEventListener("storage", displayStorageEvent, true);
+	jump2StaticPage();
+
+	function storageAppData() {
+		console.log(currentUrl)
+
+		//parse href
+		var params = window.location.search;
+		if(params) {
+			var appData = {};
+			var paramArray = params.split('&');
+			for(var appId in appIds) {
+				for(var param in paramArray) {
+					if(param.indexOf(appId + '=') == 0) {
+						var paramPair = param.split('=');
+						appData[paramPair[0]] = paramPair[1];
+						break;
+					}
+				};
+			};
+
+		}
+
+	}
+
+	function storageMonitorData() {
+		//
+	}
+
+	function jump2StaticPage() {
+
+	}
+
+}
+function test() {
+	alert(appData);
+}
+
+function createImgs() {
+
+	var imgTmp = '<a href="#" data-pds="5005">' + '<img src="http://q.i01.wimg.taobao.com/bao/uploaded/i1/T1SgWTXoVjXXahKXcY_024918.jpg_100x100.jpg" />' + '</a>';
+	imgTmp = imgTmp + '<a href="#" onclick="TBH5.getPds(this)">' + '<img src="http://q.i01.wimg.taobao.com/bao/uploaded/i1/T1SgWTXoVjXXahKXcY_024918.jpg_100x100.jpg" />' + '</a>';
+
+	$("#item_list").html(imgTmp);
+
+}
+
+function displayJsonValue() {
+	var json = {
+		'a' : 123,
+		'b' : 456
+	};
+	console.log(JSON.stringify(json));
+	var jsonStr = JSON.stringify(json);
+	jsonStr = jsonStr.replace(/[|{|}|'|"]/g, '').replace(/:/g, '=').replace(/,/g, '&');
+	console.log(jsonStr);
+}
+
+function testPropertyIn() {
+	var cody = {
+		age : 23,
+		gender : 'male'
+	};
+	for(var key in cody) {// key is a variable used to represent each property name // avoid properties inherited from the prototype chain
+		if(cody.hasOwnProperty(key)) {
+			console.log(key);
+		}
+	}
+}
+
+var testV = 'a';
+var testThis = {
+	testV : 'b',
+	func1 : function() {
+		console.log(this.testV);
+		// logs myObject
+		var func2 = function() {
+			console.log(this.testV)// logs window, and will do so from this point on
+			var func3 = function() {
+				console.log(this.testV);
+				// logs window, as it’s the head object
+			}();
+		}();
+	}
+}
